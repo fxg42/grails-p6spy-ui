63c64d2a6303612a7cfe9f0d755cc6603ea60746
François-Xavier Guillemette <francois.x.guillemette@gmail.com>
2014-01-25 14:50:28 -0500

    version 0.2-SNAPSHOT

--- P6spyUiGrailsPlugin.groovy
+++ P6spyUiGrailsPlugin.groovy
@@ -9,7 +9,7 @@ import com.p6spy.engine.common.P6SpyProperties
 
 class P6spyUiGrailsPlugin {
 
-	String version = '0.1'
+	String version = '0.2-SNAPSHOT'
 	String grailsVersion = '2.0 > *'
 	String author = 'Burt Beckwith'
 	String authorEmail = 'beckwithb@vmware.com'
